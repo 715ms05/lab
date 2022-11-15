e5e6437e1b60d0c1d4feba4b2545087fb75f6940
ITSC <ITSC@DESKTOP-61RQHUM>
2022-11-15 19:31:22 +0900

    초기 커밋.

---
+++ exam-01-02-comment/exam-01-02-comment.c
@@ -0,0 +1,27 @@
+/*
+  여러줄 주석입니다.
+  코드로 인식되지 않습니다.
+  개발자를 위한 comment 작성
+
+단축키
+전체선텍 : Ctrl + A
+복사 : Ctrl + C
+붙여넣기 : Ctrl + V
+잘라내기 : Ctrl + X
+
+주석 단축키 : Ctrl + Shift + /
+줄삭제 : Ctrl + L
+줄복사 : Ctrl + D
+
+세로편집모드 : Alt + Shift + 방향키로 선택
+세로편집모드 해제 : Esc
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello, World"); //한줄주석
+	return 0;
+}
